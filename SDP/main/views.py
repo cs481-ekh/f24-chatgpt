707780d0f26a8c09d3eb8242290a9eef3a090947
Pierce <Piercerodriguez@u.boisestate.edu>
2024-10-22 16:26:02 -0600

    user and planner are seperate logins now

--- SDP/main/views.py
+++ SDP/main/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required,user_passes_test
 from .models import SeniorDesign
 from .forms import SeniorDesignForm
 
 
-# Create your views here.
+def is_user(user):
+    return user.groups.filter(name='User').exists()
 
+def is_planner(user):
+    return user.groups.filter(name='Planner').exists()
+
+# Create your views here.
 def main(request):
     senior_designs = SeniorDesign.objects.all()
-    return render(request, 'main-page.component.html', {'senior_designs': senior_designs})
 
+    is_planner = request.user.groups.filter(name='Planner').exists()
+
+    return render(request, 'main-page.component.html', {
+        'senior_designs': senior_designs,
+        'is_planner': is_planner})
+
+@login_required
+@user_passes_test(is_user)
 def new_team_entry(request):
     if request.method == 'POST':
         form = SeniorDesignForm(request.POST)
@@ -20,6 +33,8 @@ def new_team_entry(request):
 
     return render(request, 'new-team-entry.component.html', {'form': form})
 
+@login_required
+@user_passes_test(is_user)
 def create_senior_design(request):
     if request.method == 'POST':
         form = SeniorDesignForm(request.POST, request.FILES)
@@ -30,6 +45,7 @@ def create_senior_design(request):
         form = SeniorDesignForm()
     return render(request, 'create_senior_design.html', {'form': form})
 
+@login_required
 def senior_design_list(request):
     designs = SeniorDesign.objects.all()
     return render(request, 'senior_design_list.html', {'designs': designs})
