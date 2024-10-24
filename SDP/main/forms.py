8f0db0947ae1f1bbcb20f4c070e90fa7d62ddbbb
Nolan Stetz <nolan.stetz@gmail.com>
2024-10-24 12:06:58 -0600

    closes #84

--- SDP/main/forms.py
+++ SDP/main/forms.py
@@ -4,24 +4,26 @@ from .models import SeniorDesign
 class SeniorDesignForm(forms.ModelForm):
     class Meta:
         model = SeniorDesign
-        fields = ['Team_abbreviation', 'Location', 'Poster_title', 'Abstract', 'num_team_members', 'team_member_names', 
-                  'Need_power', 'two_outlets', 'table', 'foamboard', 'clips', 'large_presentation', 
-                  'sponsor_logos', 'sponsor_logo_image', 'pictures', 'project_picture']
-        widgets = {
-            'sponsor_logo_image': forms.ClearableFileInput(attrs={'multiple': False}),
-            'project_picture': forms.ClearableFileInput(attrs={'multiple': False}),
-        }
-
+        fields = [
+            'Department',
+            'Poster_title',
+            'Abstract',
+            'num_team_members',
+            'team_member_names',
+            'Need_power',
+            'Need_more',
+            'table',
+            'easle',
+            'foam',
+            'Brief_description',
+            'Any_additional_comments',
+            'sponsor_logos',
+            'pictures',
+            'sponsor_first_last_name',
+            'sponsor_affiliation',
+            'contact_email'
+        ]
+        
     def clean(self):
         cleaned_data = super().clean()
-        sponsor_logos = cleaned_data.get('sponsor_logos')
-        sponsor_logo_image = cleaned_data.get('sponsor_logo_image')
-        pictures = cleaned_data.get('pictures')
-        project_picture = cleaned_data.get('project_picture')
-
-        if sponsor_logos and not sponsor_logo_image:
-            self.add_error('sponsor_logo_image', 'Please upload a sponsor logo image.')
-        if pictures and not project_picture:
-            self.add_error('project_picture', 'Please upload a project picture.')
-
         return cleaned_data
--- SDP/main/models.py
+++ SDP/main/models.py
@@ -1,22 +1,24 @@
 from django.db import models
 
 class SeniorDesign(models.Model):
-    Team_abbreviation = models.CharField(max_length=100, default='Team')
-    Location = models.CharField(max_length=100, default='Location')
+    Department = models.CharField(max_length=100, default='Department X')
     Poster_title = models.CharField(max_length=255, default='Title')
-    Abstract = models.TextField(default='Abstract')
+    Abstract = models.TextField(max_length=900, default='Abstract')
     num_team_members = models.IntegerField(default=1)
     team_member_names = models.TextField(default='Name')
     Need_power = models.BooleanField(default=False)
-    two_outlets = models.BooleanField(default=False)
+    Need_more = models.BooleanField(default=False)
     table = models.BooleanField(default=False)
-    foamboard = models.BooleanField(default=False)
-    clips = models.BooleanField(default=False)
-    large_presentation = models.BooleanField(default=False)
+    easle = models.BooleanField(default=False)
+    foam = models.BooleanField(default=False)
+    Brief_description = models.TextField(max_length=900, default='Abstract')
+    Any_additional_comments = models.TextField(max_length=900, default='Abstract')
     sponsor_logos = models.BooleanField(default=False)
-    sponsor_logo_image = models.ImageField(upload_to='sponsor_logos/', blank=True, null=True)
     pictures = models.BooleanField(default=False)
-    project_picture = models.ImageField(upload_to='project_pictures/', blank=True, null=True)
+    sponsor_first_last_name = models.TextField(default='Name')
+    sponsor_affiliation = models.TextField(default='Name')
+    contact_email = models.TextField(default='Name')
+    sponsor_first_last_name = models.TextField(default='Name')
 
     def __str__(self):
         return self.Team_abbreviation
